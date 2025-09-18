8862d407702070176bb1bc6e36e0d4ca28d2093e
JuliaPrin3298 <juliaprinnena1123@gmail.com>
2025-09-18 11:46:52 -0300

    Matematica

--- uniao_sem_elemento_repitido.cpp
+++ uniao_sem_elemento_repitido.cpp
@@ -3,10 +3,26 @@
 
 int main(){
 
-    int i, a[6], b[6], U[11], n;
+    int n;
+    int i, a[n], b[n], U[2*n];
+    a[1] = 2;
+    a[2] = 7;
+    a[3] = -12;
+    a[4] = 6;
+    a[5] = 5;
 
-    for(i = 1; i<=n; i++){
+    b[1] = 7;
+    b[2] = 9;
+    b[3] = 3;
+    b[4] = 2;
+    b[5] = -12;
+
+    for(i = 1; i < n; i++){
         U[i] = a[i];
         U[i+n]=b[i];
     }
+
+     for(i = 1; i <= n; i ++)
+        printf("\n %d", U[i]);
+    getch(); 
 }
