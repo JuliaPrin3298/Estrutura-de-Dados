8d7498a8f5f10e2e61d8272b783e922d2abb4548
JuliaPrin3298 <juliaprinnena1123@gmail.com>
2025-09-25 19:01:41 -0300

    Projeto 1 Matematica

--- projeto1.cpp
+++ projeto1.cpp
@@ -1,61 +1,118 @@
-#include<stdio.h>
-#include<locale.h>
-#include<conio.h>
+#include <stdio.h>
+#include <locale.h>
+#include <conio.h>
 
-int main(){
+int main()
+{
 
     setlocale(LC_ALL, "Portuguese");
 
-    int n, i, j, temp;
-    int a[10], b[10], U[n], I[n], D[n];
+    int a[10], b[10], i, j, repetido;
+    int temp;
 
-    printf("Vetor A");
-    for(i = 0; i >= 10; i ++){
-        do{
-            printf("Insira o valor %d valor ", i + 1); 
-            scanf("%d" &a[i]);
-                if(a[i] == a[i]){
-                    printf("Coloque um valor diferente! Não repita valores")
+    printf("Vetor A\n");
+
+    for (i = 0; i < 10; i++)
+    {
+        do
+        {
+            repetido = 0;
+
+            printf("Insira o %dº valor: ", i + 1);
+            scanf("%d", &a[i]);
+
+            // verifica o que foi digitado antes
+            for (j = 0; j < i; j++)
+            {
+                if (a[i] == a[j])
+                {
+                    repetido = 1;
+                    printf("Coloque um valor diferente! Não repita valores.\n");
+                    break;
                 }
-        }(a[i] != a[])
-        
+            }
+        } while (repetido == 1); // repete se o valor já existir
     }
 
-    printf("Vetor B")
-        for(i = 0; i >= 10; i ++){
-        printf("Insira o valor %d valor ", i + 1);
-        scanf("%d" &b[i]);
+    // mostra os valores
+    printf("\nValores no vetor A: ");
+    for (i = 0; i < 10; i++)
+    {
+        printf("\n A[%d] = %d ", i, a[i]);
+    }
+    printf("\n");
+
+
+    // Ordem decrescente
+    for (j = 0; j < 10 - 1; j++)
+    {
+        for (i = 0; i < 10 - j - 1; i++)
+        {
+            if (a[i] < a[i + 1])
+            {
+                temp = a[i];
+                a[i] = a[i + 1];
+                a[i + 1] = temp;
+            }
+        }
+    }
+
+    printf("Em ordem decrescente: ");
+    for (i = 0; i < 10; i++)
+    {
+        printf("\n %d ", a[i]);
+    }
+
+    //Vetor B
+    printf("\n Vetor B");
+
+    for(i = 0; i < 10; i++){
+
+        do
+        {
+            printf("\n Digite o %d° valor: ", i + 1);
+            scanf("%d", &b[i]);
+
+            repetido = 0;
+
+            for(j = 0; j < i; j++){
+                if(b[i] == b[j]){
+                    repetido = 1 ;
+                    printf("\n Coloque um valor diferente! Não repita valores.\n");
+                    break;
+                }
+            }
+
+        }while (repetido == 1);
+
+    }
+
+    printf("\nValores no vetor B: ");
+    for(i = 0; i < 10; i++) {
+        printf("\n B[%d] = %d ", i, b[i]);
     }
 
     //Ordem decrescente 
-    for(j = 0; j >= n; j++){
-        for(i = 0; i >= n; i++){
-            if(a[i] < a[i+1]){
-                temp = a[i];
-                a[i] = a[i+1];
-                a[i]= temp;
-            }
-        }
-    }
-
-    printf("Em ordem decrescente: ");
-    for(i = 0; i <qtde_numeros;i++)
-        printf("%d ", numeros[i]);
-
-    //Ordem decrescente
-
-    for(j = 0; j >= n; j++){
-        for(i = 0; i >= n; i++){
+    for(j = 0; j<10 -1; j++){
+        for(i = 0; i < 10 -j -1; i++){
             if(b[i] < b[i+1]){
                 temp = b[i];
                 b[i] = b[i+1];
-                b[i]= temp;
+                b[i+1] = temp;
             }
         }
     }
 
-    printf("Em ordem decrescente: ");
-    for(i = 0; i <qtde_numeros;i++)
-        printf("%d ", numeros[i]);
+    printf("\n Em ordem Decrescente: ");
+    for(i = 0; i <10; i++){
+        printf("\n %d", b[i]);
+    }
 
+    return 0;
 }
+
+/*
+
+    
+
+*/
